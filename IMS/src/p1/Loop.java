2147f1030c6d6da656bbb7e4bf3841b8d9d14105
QAP22 <QAP22@QAP22-PC>
2019-02-13 11:22:38 +0530

    okkk

---
+++ IMS/src/p1/Loop.java
@@ -0,0 +1,24 @@
+package p1;
+
+public class Loop {
+
+	public static void main(String[] args)throws Exception {
+		//Sum = 0;
+		//int i=1;
+		/*for(int i = 1;i<=5;i++)
+		{
+			Sum = Sum + i;
+			System.out.println(i); 
+		}*/
+		
+//System.out.println(Sum);
+	      //System.out.println("Sum = " +Sum);
+		int Sum=0;
+		int i=1;
+	      while (i<5){
+	    	 Sum=Sum+i;
+		}
+	      System.out.println("Sum = " +Sum);
+	}
+
+}
